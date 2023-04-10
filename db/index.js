7b9432bb29cb9b45c88cdbca0e8d1e92c7d5c1da
mvazquez <matias.vazquezmartin@gmail.com>
2023-04-10 19:23:21 -0300

    implementacion dao y mongoose

---
+++ db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { dbAdmin, dbPassword, dbHost } = require("../src/config/db.config");
+
+const mongoConnect = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${dbAdmin}:${dbPassword}@${dbHost}/?retryWrites=true&w=majority`
+    );
+    console.log("db conectada");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = mongoConnect;
